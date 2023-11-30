126336ce093f7f9a8e4667554d6e45c8263e295d
Armando Payan Angulo <11903392+cybershibo@users.noreply.github.com>
2023-11-30 15:04:14 -0700

    Initial Release
    Correcciones:
    -Conexion mas estable
    -Distribucion de elementos mas estetica
    -Se integran todas las salidas y entradas en un mismo codigo
    -Se crea instalador/ejecutable portable con pyinstaller

--- Proyecto final/LiveControl/PMCControl.py
+++ Proyecto final/LiveControl/PMCControl.py
@@ -7,60 +7,62 @@ from tkinter import ttk
 from tkinter import scrolledtext
 from tkinter import Entry
 
-@Gooey(program_name="Arduino Control", default_size=(600, 700), advanced=True)
+
+
+@Gooey(program_name="PMC Control GUI", default_size=(600, 700), advanced=True)
 class ArduinoGUI:
     def __init__(self, master):
         self.master = master
-        self.master.title("Arduino Control")
+        self.master.title("PMC Control GUI")
 
-        self.com_label = ttk.Label(self.master, text="Select COM Port:")
-        self.com_label.grid(row=0, column=0, pady=5, padx=10, sticky="w")
+        self.com_label = ttk.Label(self.master, text="Puerto COM:")
+        self.com_label.grid(row=0, column=0, pady=5, padx=0)
 
         self.com_var = tk.StringVar()
         self.com_entry = ttk.Combobox(self.master, textvariable=self.com_var)
-        self.com_entry.grid(row=0, column=1, pady=5, padx=10)
+        self.com_entry.grid(row=0, column=1, pady=5, padx=0)
 
-        self.refresh_button = ttk.Button(self.master, text="Refresh Ports", command=self.refresh_serial_ports)
-        self.refresh_button.grid(row=0, column=2, pady=5, padx=10)
+        self.refresh_button = ttk.Button(self.master, text="Actualizar", command=self.refresh_serial_ports)
+        self.refresh_button.grid(row=1, column=3, pady=5, padx=10)
 
-        self.connect_button = ttk.Button(self.master, text="Connect", command=self.connect_to_arduino)
+        self.connect_button = ttk.Button(self.master, text="Conectar", command=self.connect_to_arduino)
         self.connect_button.grid(row=0, column=3, pady=5, padx=10)
 
-        self.get_inputs_button = ttk.Button(self.master, text="Get Inputs", command=self.get_inputs)
-        self.get_inputs_button.grid(row=0, column=4, pady=5, padx=10)
+        self.get_inputs_button = ttk.Button(self.master, text="Act. Entradas", command=self.get_inputs)
+        self.get_inputs_button.grid(row=2, column=3, pady=5, padx=10)
 
         self.pin_states = ["LOW"] * 8
         self.programmable_states = [False] * 12
 
         for i in range(8):
-            btn_text = f"Output Pin {i}"
+            btn_text = f"Salida digital {i}"
             btn = ttk.Button(self.master, text=btn_text, command=lambda index=i: self.toggle_output_pin(index))
             btn.grid(row=i + 1, column=0, pady=5, padx=10)
 
         for i in range(8, 20):
-            btn_text = f"Programmable Pin {i - 8}"
+            btn_text = f"Pin Programable {i - 8}"
             btn = ttk.Button(self.master, text=btn_text, command=lambda index=i: self.toggle_programmable_pin(index - 8))
-            btn.grid(row=i - 7, column=1, pady=5, padx=10)
+            btn.grid(row=i - 7, column=1, pady=5, padx=0)
 
         self.entry_labels = []
         self.entry_vars = []
 
         for i in range(8):
-            label_text = f"Input {i} Value:"
+            label_text = f"Entrada digital {i}:"
             entry_label = ttk.Label(self.master, text=label_text)
-            entry_label.grid(row=15 + i, column=0, pady=5, padx=10, sticky="w")
+            entry_label.grid(row=1 + i, column=2, pady=5, padx=10, sticky="w")
             self.entry_labels.append(entry_label)
 
             entry_var = tk.StringVar()
             entry = Entry(self.master, textvariable=entry_var, state='readonly')
-            entry.grid(row=15 + i, column=1, pady=5, padx=10)
+            entry.grid(row=1 + i, column=2, pady=5, padx=100)
             self.entry_vars.append(entry_var)
 
-        self.terminal_label = ttk.Label(self.master, text="Terminal:")
-        self.terminal_label.grid(row=23, column=0, pady=5, padx=10)
+        #self.terminal_label = ttk.Label(self.master, text="Terminal:")
+        #self.terminal_label.grid(row=23, column=2, pady=5, padx=10)
 
-        self.terminal_text = scrolledtext.ScrolledText(self.master, height=10, width=40, wrap=tk.WORD)
-        self.terminal_text.grid(row=24, column=0, pady=5, padx=10, columnspan=4)
+        self.terminal_text = scrolledtext.ScrolledText(self.master, height=10, width=80, wrap=tk.WORD)
+        self.terminal_text.grid(row=24, column=0, pady=5, padx=25, columnspan=4)
 
         self.ser = None
 
@@ -76,12 +78,12 @@ class ArduinoGUI:
             port = self.com_var.get()
             if port:
                 self.ser = serial.Serial(port, 9600, timeout=2)
-                self.log_to_terminal("Connected to Arduino")
+                self.log_to_terminal("Conectado a Portenta")
                 self.auto_refresh_serial_ports()  # Start automatic refresh
             else:
-                self.log_to_terminal("Please select a valid COM port.")
+                self.log_to_terminal("No se ha seleccionado puerto COM")
         except Exception as e:
-            self.log_to_terminal(f"Error connecting to Arduino: {str(e)}")
+            self.log_to_terminal(f"Error al conectarse a la placa: {str(e)}")
 
     def toggle_output_pin(self, pin_index):
         current_state = self.pin_states[pin_index]
@@ -104,7 +106,7 @@ class ArduinoGUI:
                 self.ser.write(command_str.encode())
                 self.log_to_terminal(f"Sent command: {command_str}")
             else:
-                self.log_to_terminal("Not connected to Arduino. Please connect first.")
+                self.log_to_terminal("No hay conexión con la placa. Por favor, conecte primero.")
         except Exception as e:
             self.log_to_terminal(f"Error sending command: {str(e)}")
 
@@ -122,7 +124,7 @@ class ArduinoGUI:
     def auto_refresh_serial_ports(self):
         self.refresh_serial_ports()
         self.read_input_states()
-        self.master.after(1000, self.auto_refresh_serial_ports)  # Refresh every 5 seconds
+        self.master.after(500, self.auto_refresh_serial_ports)  # Refresh every 5 seconds
 
     def read_input_states(self):
         try:
@@ -149,14 +151,15 @@ class ArduinoGUI:
                     else:
                         buffer += char
             else:
-                self.log_to_terminal("Not connected to Arduino. Please connect first.")
+                self.log_to_terminal("No hay conexion a la placa.")
         except Exception as e:
             self.log_to_terminal(f"Error reading input states: {str(e)}")
 
 if __name__ == "__main__":
-    parser = GooeyParser(description="Arduino Control GUI")
+    parser = GooeyParser(description="PMC Control GUI")
     args = parser.parse_args()
     
     root = tk.Tk()
     app = ArduinoGUI(root)
     root.mainloop()
+
